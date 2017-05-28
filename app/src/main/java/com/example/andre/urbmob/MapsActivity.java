d06e3b664ecf62fc6e53daa0bda7ad4a04c78d33
André victor <andrevnl@hotmail.com>
2017-05-28 09:34:39 -0300

    Alterações na classe de permissão
    Alterações na classe de permissão

--- app/src/main/java/com/example/andre/urbmob/MapsActivity.java
+++ app/src/main/java/com/example/andre/urbmob/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.andre.urbmob;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -26,22 +28,25 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import static com.example.andre.urbmob.R.string.app_name;
 
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,LocationListener{
-
-    GoogleApiClient mGoogleApiClient;
-
-    //Mapa
+    //Classe responsavel por conectar a API do Google
+    private GoogleApiClient mGoogleApiClient;
+    //Classe responsavel por criar o Mapa
     private GoogleMap mMap;
-
+    //Classe responsavel por gerenciar o GPS
     LocationRequest locationRequest;
-
+    //LatLng para pegar a latitude e a longitude da localização atual
     private LatLng loc = null;
-
-    Location l;
-
+    //Location com a localização atual
+    Location currentLocation;
+    //String com as permissões
     String[] permissoes = new String[]{
             android.Manifest.permission.ACCESS_FINE_LOCATION,
             android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_NETWORK_STATE,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
 
     @Override
@@ -52,23 +57,112 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //Chama a classe de permissoes para validacao
         PermissionUtils.validate(this,0,permissoes);
 
+        //Configuração da API LocationServices do Google
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
 
+        //Configuração da chamada do GPS
         locationRequest = new LocationRequest();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
+        locationRequest.setInterval(10000); //Intervalo entre as chamadas é de 10 segundos
+        locationRequest.setFastestInterval(5000); //Não recebe uma nova solicitação do GPS se não passar ao menos 5 segundos
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Obtem um SupportMapFragment e avisa quando esta tudo pronto
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    protected void onStart() {
+        //Conecta ao serviço LocationServices
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        //Desconecta do serviço LocationServices
+        mGoogleApiClient.disconnect();
+        //Desativa o GPS para não atualizar o local
+        stopLocationUpdates();
+        super.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        try {
+            currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if (currentLocation == null){
+               // Toast.makeText(this, "Não foi possivel determinar sua localização", Toast.LENGTH_LONG).show();
+                loc = new LatLng(-23.5631338 , -46.6543286); //Seta ponto inicial no mapa
+                mMap.addMarker(new MarkerOptions().position(loc).title("Avenida Paulista"));
+            } else{
+                loc = new LatLng(currentLocation.getLatitude() , currentLocation.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(loc).title("Sua localização atual"));
+            }
+            //Localização atual do proprio Google Maps
+            mMap.setMyLocationEnabled(true);
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc,14));
+
+        }catch (SecurityException e){
+            Toast.makeText(this, "Não foi possivel determinar sua localização", Toast.LENGTH_LONG).show();
+            Log.d("Erro", "Exceção : "+ e );
+        }
+    }
+
+    public void AdicionarRota(View view){
+        Intent
+    }
+
+    protected void startLocationUpdates(){
+        try{
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
+        }catch (SecurityException e){
+            Log.d("Erro", "Exceção : "+ e);
+        }
+
+    }
+
+    protected void stopLocationUpdates(){
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        startLocationUpdates(); //Inicia o GPS quando conecta na API
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d("estado", "suspenso");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("estado", "erro de conexão");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        loc = new LatLng(latitude,longitude);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(loc,14);
+        mMap.animateCamera(update);
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(loc).title("Sua localização atual"));
+    }
+
+    //O proprio Android tem que chamar esse metodo caso precise de alguma permissao
     @Override
     public void onRequestPermissionsResult(int requestCode,String[] permissions, int[] grantResults){
         super.onRequestPermissionsResult(requestCode,permissions,grantResults);
@@ -97,82 +191,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             AlertDialog dialog = builder.create();
             dialog.show();
         }
-    }
-
-    @Override
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        stopLocationUpdates();
-        super.onStop();
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        try {
-            l = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-
-            if (l == null){
-              //  Toast.makeText(this, "Não foi possivel determinar sua localização", Toast.LENGTH_LONG).show();
-                loc = new LatLng(-23.5631338 , -46.6543286);
-            } else{ loc =
-                    new LatLng(l.getLatitude() ,
-                            l.getLongitude());
-            }
-
-            mMap.setMyLocationEnabled(true);
-            mMap.addMarker(new MarkerOptions().position(loc).title("Sua localização atual"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc,14));
-        }catch (SecurityException e){
-
-        }
-    }
-
-    public void AtualizarCameraDoMapa(View view){
-        if(mMap != null && l == null){
-            loc = new LatLng(-40,-48);//l.getLatitude(), l.getLongitude());
-            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(loc,14);
-            mMap.animateCamera(update);
-        }
-    }
-
-    protected void startLocationUpdates(){
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
-    }
-
-    protected void stopLocationUpdates(){
-        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
-    }
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        startLocationUpdates(); //Inicia o GPS
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.d("estado", "suspenso");
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d("estado", "erro");
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-        loc = new LatLng(latitude,longitude);
-        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(loc,14);
-        mMap.animateCamera(update);
     }
 }
